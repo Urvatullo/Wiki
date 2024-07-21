0d7099732601468c8d30bfbdee78337cbe1bfb6c
Urvatullo Atoev <urvatulloatoev@gmail.com>
2024-07-21 21:57:24 +0300

    Added getnews json

--- scripts/index.js
+++ scripts/index.js
@@ -71,7 +71,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 newsRow.appendChild(div);
             });
         })
-        .catch(error => console.error('Error fetching news:', error));
+    .catch(error => console.error('Error fetching news:', error));
 
     fetch('data/articles.json')
         .then(response => response.json())
@@ -98,7 +98,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 articlesRow.appendChild(div);
             });
         })
-        .catch(error => console.error('Error fetching articles:', error));
+    .catch(error => console.error('Error fetching articles:', error));
 
     findButton.addEventListener('click', filterOrganizations);
     browseButton.addEventListener('click', filterOrganizations);
